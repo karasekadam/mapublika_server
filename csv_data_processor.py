878d3a12f1c524ae2916a3eac927f523d20e5aa1
karasekadam <karasekad@gmail.com>
2022-10-08 00:44:56 +0200

    fda

--- csv_data_processor.py
+++ csv_data_processor.py
@@ -1,4 +1,16 @@
 import pandas as pd
+import numpy as np
 
 df = pd.read_csv("uzemi_ciselniky.csv")
-print(df.to_string())
+#print(df.to_string())
+
+#def parse_file(path_to_file, value="hodnota", value_name="pocetdeti_txt", localization="", localization_code="0"):
+
+
+def gib_header_print(path_to_file):
+    #pd.DataFrame(np.random.randn(10, 4), columns=list('abcd')).to_csv(path_to_file, mode='w')
+    #pd.read_csv(path_to_file, index_col=0, nrows=0).columns.tolist()
+    print(df.columns)
+
+if __name__ == '__main__':
+    gib_header_print("uzemi_ciselniky.csv")
